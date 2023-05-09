43990d4fba556e348a039b19486baca30c1cd60f
Paul Reisenberg <paul.reisenberg@web.de>
2023-05-09 15:16:48 +0200

    initial commit of basic spring boot application that returns hello world on port 8080

---
+++ src/main/java/api/rest/HelloWorldController.java
@@ -0,0 +1,15 @@
+package api.rest;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+
+
+    @GetMapping("/")
+    public String helloWorld(){
+        return "Hello World";
+    }
+
+}
